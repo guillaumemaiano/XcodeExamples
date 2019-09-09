78e28aa1fee4c836c995b7a7727cdfb9c89c6221
Guillaume Maiano <guillaume@maiano.fr>
2019-09-09 03:24:08 +0200

    34:30 in video, commented out my little addition to get the same layout

--- SwiftUI_WWDC_Example/SwiftUI_WWDC_Example/RoomDetail.swift
+++ SwiftUI_WWDC_Example/SwiftUI_WWDC_Example/RoomDetail.swift
@@ -1,43 +1,53 @@
  //
-//  RoomDetail.swift
-//  SwiftUI_WWDC_Example
-//
-//  Created by guillaume MAIANO on 09/09/2019.
-//  Copyright © 2019 guillaume MAIANO. All rights reserved.
-//
-
-import SwiftUI
-
-struct RoomDetail: View {
+ //  RoomDetail.swift
+ //  SwiftUI_WWDC_Example
+ //
+ //  Created by guillaume MAIANO on 09/09/2019.
+ //  Copyright © 2019 guillaume MAIANO. All rights reserved.
+ //
+ 
+ import SwiftUI
+ 
+ struct RoomDetail: View {
     let room: Room
-// @state (source of truth variable) is a mechanism managed by UISwift itself to redraw the body if needed
-// oh, I so hope I'll understand the idea again whenever I check this code out
-// Somewhere around 23:00 in WWDC 2019-204
+    // @state (source of truth variable) is a mechanism managed by UISwift itself to redraw the body if needed
+    // oh, I so hope I'll understand the idea again whenever I check this code out
+    // Somewhere around 23:00 in WWDC 2019-204
     @State private var zoomed = false
     
     var body: some View {
-        VStack {
-            // only the Observation Deck image is available as an asset
-            Image(room.imageName)
-                .resizable()
-                .aspectRatio(contentMode: zoomed ? .fill : .fit)
-                // was .tapAction in WWDC 2019-204
-                .onTapGesture { withAnimation {
-                    self.zoomed.toggle() }
-            }
-                Text("Capacity: \(room.capacity) people").italic()
-                // Amazingly, the navbar call works from anywhere within the view
-                    .navigationBarTitle(Text(room.name), displayMode: .inline)
+       // VStack {
+            // WWDC video was topLeading, but I like it better topTrailing. Whatev'
+            ZStack(alignment: .topTrailing) {
+                // only the Observation Deck image is available as an asset
+                Image(room.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                    // was .tapAction in WWDC 2019-204
+                    .onTapGesture { withAnimation {
+                        self.zoomed.toggle() }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                
+                Image(systemName: "video.fill")
+                    // SF images use the current font to size themselves, so adapt the font to change the image size!
+                    .font(.title)
+                    // Avoid the camera image being right close to the edge
+                    .padding(.all)
+//            }
+//            Text("Capacity: \(room.capacity) people").italic()
+//                // Amazingly, the navbar call works from anywhere within the view
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
         }
     }
-}
-
-#if DEBUG
-struct RoomDetail_Previews: PreviewProvider {
+ }
+ 
+ #if DEBUG
+ struct RoomDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-        RoomDetail(room: testData[0])
+            RoomDetail(room: testData[0])
         }
     }
-}
-#endif
+ }
+ #endif
