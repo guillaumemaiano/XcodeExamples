915e878964cf25f68ccad5fdd8dfa91ce5a2597b
GEM <guillaume@maiano.fr>
2019-09-18 14:52:09 +0200

    End of combining views tutorial

--- AppleSwiftUIOnlineTutorials/Landmarks/Landmarks/Badge.swift
+++ AppleSwiftUIOnlineTutorials/Landmarks/Landmarks/Badge.swift
@@ -3,18 +3,50 @@
 //  Landmarks
 //
 //  Created by guillaume MAIANO on 17/09/2019.
-//  Copyright © 2019 guillaume MAIANO. All rights reserved.
 //
 
 
 import SwiftUI
 
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 25))
+    }
+}
+
 struct Badge: View {
+    // Apple made it a static var, but I'd rather pass in the amount of points myself...
+    var points: Int = 8
+
+    var badgeSymbols: some View {
+        ForEach(0..<points) { rotation in
+          
+            return RotatedBadgeSymbol(angle: .degrees(Double(rotation) / Double(self.points)) * 360.0)
+        }
+            .opacity(0.5)
+    }
+
     var body: some View {
         ZStack {
             BadgeBackground()
-            BadgeSymbol()
-        }
+            // Geometry reader checks the surrounding geometry
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }.scaledToFit()
     }
 }
 
---
+++ AppleSwiftUIOnlineTutorials/Landmarks/Landmarks/ProfileHost.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by guillaume MAIANO on 18/09/2019.
+//  Copyright © 2019 guillaume MAIANO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
